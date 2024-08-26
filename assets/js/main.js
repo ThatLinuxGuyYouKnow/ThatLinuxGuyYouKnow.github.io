9400e4cd2e4ffa90e49101b7119f35f69a3dfd1a
Alabi Ayobami <arocket04@gmail.com>
2024-08-27 00:02:46 +0100

    added scroll animations back in

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,52 @@ const body = document.body;
 // init scrollmagic controller
 const scrollmagicController = new ScrollMagic.Controller();
 
+// cursor look and behavior
+const cursorPointer = body.appendChild(createElement('div'));
+const cursorRing = cursorPointer.appendChild(createElement('div'));
+const cursor = {
+    x: 0,
+    y: 0,
+    ringX: 0,
+    ringY: 0
+};
+cursorPointer.classList.add('cursor-pointer');
+cursorRing.classList.add('cursor-ring');
+
+body.addEventListener('mousemove', (ev) => {
+    cursor.x = ev.clientX;
+    cursor.y = ev.clientY;
+});
+
+gsap.to({}, 0.010, {
+    repeat: -1,
+    onRepeat: () => {
+        cursor.ringX += (cursor.x - cursor.ringX) / 6;
+        cursor.ringY += (cursor.y - cursor.ringY) / 6;
+        gsap.set(cursorRing, {
+            css: {
+                left: cursor.ringX - 15,
+                top: cursor.ringY - 15
+            }
+        });
+        gsap.set(cursorPointer, {
+            css: {
+                left: cursor.x,
+                top: cursor.y
+            }
+        });
+    }
+});
+
+selectAllElements('.cursor-animate').forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        cursorPointer.classList.add('hover');
+    });
+    el.addEventListener('mouseleave', () => {
+        cursorPointer.classList.remove('hover');
+    });
+});
+
 // navbar toggle
 const navbarMenu = selectElement('.navbar__menu');
 selectElement('.navbar__toggle').addEventListener('click', () => {
